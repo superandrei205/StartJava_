ea1fa5c3f709aa651c758635687fcd83b6e10526
andreiremizov <ARemizov@inno.tech>
2026-01-09 13:23:04 +0400

    fix comments

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -15,6 +15,26 @@ public class Calculator {
         this.secondNumber = secondNumber;
     }
 
+    public static double power(int base, int exponent) {
+        if (base == 0 && exponent == 0) {
+            return 1.0;
+        }
+
+        if (base == 0 && exponent < 0) {
+            throw new ArithmeticException("0 cannot be raised to a negative power");
+        }
+
+        if (exponent == 0) {
+            return 1.0;
+        }
+
+        if (exponent > 0) {
+            return base * power(base, exponent - 1);
+        } else {
+            return 1.0 / power(base, -exponent);
+        }
+    }
+
     public double calculate() {
         if (secondNumber == 0 && (operation == '/' || operation == '%')) {
             System.out.println("Ошибка: деление на ноль запрещено");
@@ -26,7 +46,7 @@ public class Calculator {
             case '-' -> firstNumber - secondNumber;
             case '*' -> firstNumber * secondNumber;
             case '/' -> (double) firstNumber / secondNumber;
-            case '^' -> Math.pow(firstNumber, secondNumber);
+            case '^' -> power(firstNumber, secondNumber);
             case '%' -> firstNumber % secondNumber;
             default -> {
                 System.out.println("Ошибка: операция '" + operation + "' не поддерживается");
--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -12,9 +12,73 @@ public class GuessNumberTest {
         Player player1 = new Player(name1);
         Player player2 = new Player(name2);
 
-        GuessNumber game = new GuessNumber(player1, player2);
-        game.startGame();
+        GuessNumber game = new GuessNumber();
 
+        boolean continueGame = true;
+        while (continueGame) {
+            System.out.println("\nКомпьютер загадал число от 1 до 100. Удачи!");
+
+            boolean guessed = false;
+            while (!guessed) {
+                guessed = makeTurn(scanner, player1, game);
+                if (!guessed) {
+                    guessed = makeTurn(scanner, player2, game);
+                }
+            }
+
+            continueGame = askToContinue(scanner);
+            if (continueGame) {
+                game.createNewNumber();
+            }
+        }
+
+        System.out.println("Спасибо за игру!");
         scanner.close();
+    }
+
+    private static boolean makeTurn(Scanner scanner, Player player, GuessNumber game) {
+        int number = enterNumber(scanner, player);
+        player.setNumber(number);
+
+        if (game.isCorrect(number)) {
+            System.out.println("Поздравляем, " + player.getName() +
+                    "! Вы угадали число: " + game.getSecretNumber());
+            return true;
+        } else {
+            System.out.println(game.isLess(number) ? "Больше!" : "Меньше!");
+            return false;
+        }
+    }
+
+    private static int enterNumber(Scanner scanner, Player player) {
+        while (true) {
+            System.out.print(player.getName() + ", введите число от 1 до 100: ");
+            String input = scanner.nextLine().trim();
+
+            try {
+                int number = Integer.parseInt(input);
+                if (number >= 1 && number <= 100) {
+                    return number;
+                }
+                System.out.println("Число должно быть в диапазоне [1, 100]. Попробуйте снова.");
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка: введите целое число.");
+            }
+        }
+    }
+
+    private static boolean askToContinue(Scanner scanner) {
+        while (true) {
+            System.out.print("Хотите продолжить игру? [yes/no]: ");
+            String answer = scanner.nextLine().trim().toLowerCase();
+
+            if ("yes".equals(answer)) {
+                return true;
+            }
+            if ("no".equals(answer)) {
+                return false;
+            }
+            System.out.println("Пожалуйста, введите 'yes' или 'no'.");
+        }
     }
 }
